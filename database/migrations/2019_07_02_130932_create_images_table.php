82ed14b0363889375d01d63cee3efbb396f44d5f
Quang Tran H <quang.tran@asiantech.vn>
2019-07-02 20:15:43 +0700

    sua migration

--- database/migrations/2019_07_02_130932_create_images_table.php
+++ database/migrations/2019_07_02_130932_create_images_table.php
@@ -16,7 +16,7 @@ class CreateImagesTable extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('ten_image');
             $table->timestamps();
         });
     }
